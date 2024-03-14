8b05ea148c88b6ebfd1d357e5506e15a2591b05c
Abinav Prasad <55477176+Abinav160701@users.noreply.github.com>
2024-03-14 13:57:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import numpy as np
 import tensorflow as tf
 from annoy import AnnoyIndex
 import pandas as pd
+from detectron2.change_bg import get_bg_changed
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from sklearn.metrics.pairwise import cosine_similarity
@@ -17,6 +18,7 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from Preprocess import process_query_image_mc
 from models import load_model_from_db
 from load_feature_vec import load_features_from_db
+from Opensearchfn import get_similar_skus
 
 
 
@@ -34,7 +36,12 @@ with st.sidebar:
     # st.[element_name]
     l1 = st.selectbox('Choose l1 category',('Men', 'Women', 'Kids'))
     l2 = st.selectbox('Choose l2 category',('Clothing', 'Shoes', 'Bags'))
-    test_image = st.file_uploader("Choose a file")
+    picture=None
+    #picture = st.camera_input("Take a picture") 
+    if picture:
+        test_image=picture
+    else:
+        test_image = st.file_uploader("Choose a file")
     if test_image is not None:
         # To read file as bytes:
         # bytes_data = uploaded_file.getvalue()
@@ -63,10 +70,20 @@ def get_image(d):
         return "https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-media-1677509740"
     
 
-def find_sim_images(query_features,features,labels,df):
-    similarities = cosine_similarity(query_features, np.array(features))
-    top_indices = np.argsort(similarities[0])[::-1][:]  # Adjust the number as needed
+def find_sim_images(query_features,df):
+    
+   #similarities = cosine_similarity(query_features, np.array(features))
+   #top_indices = np.argsort(similarities[0])[::-1][:]   Adjust the number as needed
+    skus=get_similar_skus(query_features)
     l=[]
+    st.write(skus[0])
+    for i in skus:
+        l.append(i)
+    d=df.query(f"sku in {l}")
+    for i in range(10):
+        st.write("class:",skus[i][:])
+        st.image(get_image(df.query(f"sku=='{skus[i][:]}'").reset_index(drop=True)),width=100)
+    '''l=[]
 #    for i in top_indices:
 #        if labels[i]=='TH6-MW0MW307050A4':
 #            st.write(np.where(top_indices==i))
@@ -79,8 +96,8 @@ def find_sim_images(query_features,features,labels,df):
 #    for i in top_indices:
 #        if i not in top_indices_unique:
 #            top_indices_unique.append(i)
-#            l.append(labels[i])
-    d=df.query(f"sku in {labels_unique}")
+#            l.append(labels[i])'''
+    '''d=df.query(f"sku in {labels_unique}")
     for i in top_indices_unique[:20]:
         if '/' in labels[i]:
             st.write("class:",labels[i][:-12],"similarity:",similarities[0][i])
@@ -89,7 +106,8 @@ def find_sim_images(query_features,features,labels,df):
             st.write("class:",labels[i][:],"similarity:",similarities[0][i])
             x=labels[i]
         #st.image(get_image(d.query(f"sku=='{x}'").reset_index(drop=True)))
-        st.image(get_image(df.query(f"sku=='{x}'").reset_index(drop=True)),width=100)
+        st.image(get_image(df.query(f"sku=='{x}'").reset_index(drop=True)),width=100)'''
+    
 
 
 
@@ -138,12 +156,14 @@ if test_image is not None:
     # query_image_path = 'query/img_test1.jpeg'
     # Load the saved features and model
     query_image_path = test_image
+    print(query_image_path)
+    #white_img=get_bg_changed(query_image_path)
     query_img = process_query_image_mc(query_image_path)
     query_features = model.predict(query_img)
     # st.write(query_features)
     # Load the Annoy index
 
-    find_sim_images(query_features,features,labels,df)
+    find_sim_images(query_features,df)
     #find_similar_images_annoy(query_features[0], annoy_index, labels,df, top_n=20)
     #similar_images=find_similar_images_annoy(query_features[0], annoy_index, labels)
     #display_annoy_results(similar_images,df)
@@ -152,4 +172,6 @@ if test_image is not None:
     #top_indices = np.argsort(similarities[0])[::-1][:20]  # Adjust the number as needed
 else :
     st.write('waiting for test image....')
+
+
 
